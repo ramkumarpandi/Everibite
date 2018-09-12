b1bceda88b135faa0930af9cafdf61d62a714db8
ramkumarpandi <ram_kumar_p@rediffmail.com>
2018-09-12 17:20:35 +0530

    Add files via upload

--- src/Search.js
+++ src/Search.js
@@ -9,16 +9,23 @@ const Search = () =>
 (
 
 <div className="searchBox">
-
 	<section className="container formBorder">
 	<form className="px-5 m-5 row">
-	<div className="form-group-lg col-md-3 mr-5 pr-0 bg-white border border-success" style={{borderRadius:'3px'}}>
-      <img src={loc} alt="locIcon"/>
-  <select className="form-control border-0 d-inline-block" id="sel1" style={{width:'90%'}}>
-    <option>--Select Area--</option>
-    <option>Area1</option>
-    <option>Area2</option>
-    <option>Area3</option>
+	<div className="form-group-lg col-md-3 mr-5 px-0 selectBox">
+      <img src={loc} alt="locIcon" className="pb-2"/>
+  <select className="form-control border-0 d-inline-block p-0" id="sel1" style={{width:'225px'}}>
+    <option><span className="capital">Bengaluru, </span>
+    <span className="state">Karnataka</span>
+    </option>
+    <option><span className="capital">Chennai, </span>
+    <span className="state">TamilNadu</span>
+    </option>
+    <option><span className="capital">Thiruvanthapuram, </span>
+    <span className="state">Kerala</span>
+    </option>
+    <option><span className="capital">Hyderabad, </span>
+    <span className="state">AndhraPradesh</span>
+    </option>
   </select>
 </div>
 
@@ -29,7 +36,8 @@ const Search = () =>
             placeholder="Search for Recipes, Cusines, Restaurants .."
 		className="border-0"/>
 </FormGroup><Button className="btn-success p-3 float-right my-3" bsSize="large">FIND BITES</Button></div>
-      </form></section>
+      </form>
+      </section>
 
 </div>
 
